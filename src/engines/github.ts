956ae7c905a2f9df108881ee7dfb2fd51853e13e
Art Chaidarun <artchaidarun@gmail.com>
2020-06-05 15:17:20 -0400

    Rate limit GitHub calls

--- src/engines/github.ts
+++ src/engines/github.ts
@@ -1,83 +1,85 @@
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 
-import { Engine } from "./index";
+import { Engine, rateLimit } from "./index";
 
-let client: AxiosInstance | undefined;
+interface Repo {
+  description: null | string;
+  isArchived: boolean;
+  isFork: boolean;
+  name: string;
+}
+
+let getRepos: (() => Promise<Set<Repo>>) | undefined;
 let org: string | undefined;
 
 const engine: Engine = {
   id: "github",
   init: ({ organization, token }: { organization: string; token: string }) => {
-    client = axios.create({
+    const client = axios.create({
       baseURL: "https://api.github.com",
       headers: { Authorization: `bearer ${token}` },
     });
+
+    getRepos = rateLimit(async () => {
+      let cursor: string | undefined;
+      const repos = new Set<Repo>();
+      while (true) {
+        const {
+          data,
+        }: {
+          data?: {
+            organization: {
+              repositories: {
+                edges: { node: Repo }[];
+                pageInfo: { endCursor: string; hasNextPage: boolean };
+              };
+            };
+          };
+        } = (
+          await client.post(
+            "/graphql",
+            JSON.stringify({
+              query: `query {
+      organization(login: "${org}") { repositories(first: 100${
+                cursor ? `, after: "${cursor}"` : ""
+              }) {
+          edges { node { description isArchived isFork name } }
+          pageInfo { endCursor hasNextPage }
+      } } }`,
+            }),
+          )
+        ).data;
+
+        if (!data) {
+          break;
+        }
+
+        const { edges, pageInfo } = data.organization.repositories;
+        edges.map(e => e.node).forEach(r => repos.add(r));
+
+        if (pageInfo.hasNextPage) {
+          cursor = pageInfo.endCursor;
+        } else {
+          break;
+        }
+      }
+
+      return repos;
+    }, 60);
     org = organization;
   },
   search: async q => {
-    if (!(client && org)) {
+    if (!(getRepos && org)) {
       throw Error("Client not initialized");
     }
 
-    interface Repo {
-      description: null | string;
-      isArchived: boolean;
-      isFork: boolean;
-      name: string;
-    }
-
-    let cursor: string | undefined;
-    const repos = new Set<Repo>();
-    while (true) {
-      const {
-        data,
-      }: {
-        data?: {
-          organization: {
-            repositories: {
-              edges: { node: Repo }[];
-              pageInfo: { endCursor: string; hasNextPage: boolean };
-            };
-          };
-        };
-      } = (
-        await client.post(
-          "/graphql",
-          JSON.stringify({
-            query: `query {
-    organization(login: "${org}") { repositories(first: 100${
-              cursor ? `, after: "${cursor}"` : ""
-            }) {
-        edges { node { description isArchived isFork name } }
-        pageInfo { endCursor hasNextPage }
-    } } }`,
-          }),
-        )
-      ).data;
-
-      if (!data) {
-        break;
-      }
-
-      const { edges, pageInfo } = data.organization.repositories;
-      edges
-        .map(e => e.node)
-        .filter(
-          r =>
-            !r.isArchived &&
-            !r.isFork &&
-            (r.description?.includes(q) || r.name.includes(q)),
-        )
-        .forEach(r => repos.add(r));
-
-      if (pageInfo.hasNextPage) {
-        cursor = pageInfo.endCursor;
-      } else {
-        break;
-      }
-    }
-
-    return Array.from(repos)
+    return Array.from(await getRepos())
+      .filter(
+        r =>
+          !r.isArchived &&
+          !r.isFork &&
+          (r.description?.includes(q) || r.name.includes(q)),
+      )
       .sort((a, b) => (a.name > b.name ? 1 : -1))
       .map(r => ({
         snippet: r.description || undefined,
--- src/engines/index.ts
+++ src/engines/index.ts
@@ -18,5 +18,30 @@ export interface Result {
 
 export const escapeQuotes = (s: string) => s.replace(/"/g, '\\"');
 
+/**
+ * Wraps a function so that it returns the last invocation's cached result if
+ * called multiple times during a cooldown period of N minutes.
+ *
+ * The wrapped function must be parameterless since the cached result would be
+ * not just stale but also incorrect in the case that different parameter
+ * values were provided to the current call and the cached call.
+ */
+export const rateLimit = <R, F extends () => R>(
+  f: F,
+  intervalMinutes: number,
+): F => {
+  let lastRunAt = -Infinity;
+  let lastResult: any;
+  return ((() => {
+    const now = Date.now();
+    if (now < lastRunAt + intervalMinutes * 60 * 1000) {
+      return lastResult;
+    }
+    lastRunAt = now;
+    lastResult = f();
+    return lastResult;
+  }) as unknown) as F;
+};
+
 const engines: Engine[] = [confluence, github, hound, jenkins, jira];
 export default engines;
