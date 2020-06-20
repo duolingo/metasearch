0e1ee152a311b122fff92f0a906d0227b8af8fee
Art Chaidarun <artchaidarun@gmail.com>
2020-06-20 17:54:42 -0400

    Show engine names as result headings

--- src/ui/ui.tsx
+++ src/ui/ui.tsx
@@ -1,7 +1,12 @@
 // Can't use import/require in this source file since its compiled JS is loaded
 // directly into the browser
 
-const { useEffect, useState } = React;
+const { useEffect, useReducer, useState } = React;
+
+interface ResultGroup {
+  engineId: string;
+  results: Result[];
+}
 
 /** Converts an object to a query string that includes a cache-busting param */
 const querify = (params: Record<string, string> = {}) =>
@@ -28,57 +33,84 @@ const Header = ({ onSearch }: { onSearch: (q: string) => any }) => {
   );
 };
 
-const Sidebar = ({ engines }: { engines: Engine[] }) => (
+const Sidebar = ({ engines }: { engines: Record<string, Engine> }) => (
   <div className="sidebar">
     <ul>
-      {engines.map(engine => (
-        <li key={engine.id}>{engine.name}</li>
-      ))}
+      {Object.values(engines)
+        .sort((a, b) => (a.name > b.name ? 1 : -1))
+        .map(engine => (
+          <li key={engine.id}>{engine.name}</li>
+        ))}
     </ul>
   </div>
 );
 
-const Results = ({ results }: { results: Result[] }) => (
+const Results = ({
+  engines,
+  resultGroups,
+}: {
+  engines: Record<string, Engine>;
+  resultGroups: ResultGroup[];
+}) => (
   <div className="results">
-    {results.map((result, i) => (
-      <div key={i}>
-        <a href={result.url}>{result.title}</a>
-        <p>{result.snippet}</p>
-      </div>
-    ))}
+    {resultGroups
+      .filter(rg => rg.results.length)
+      .map(resultGroup => (
+        <div key={resultGroup.engineId}>
+          <h2>{engines[resultGroup.engineId].name}</h2>
+          {resultGroup.results.map((result, i) => (
+            <div key={i}>
+              <a href={result.url}>{result.title}</a>
+              <p>{result.snippet}</p>
+            </div>
+          ))}
+        </div>
+      ))}
   </div>
 );
 
 const App = () => {
-  const [engines, setEngines] = useState<Engine[]>([]);
-  const [results, setResults] = useState<Result[]>([]);
+  const [engines, setEngines] = useState<Record<string, Engine>>({});
+  const [resultGroups, dispatch] = useReducer(
+    (state: ResultGroup[], action: ResultGroup | undefined) =>
+      action ? [...state, action] : [],
+    [],
+  );
 
   useEffect(() => {
     (async () =>
-      setEngines(
-        Object.values<Engine>(
-          await (await fetch(`/api/engines?${querify()}`)).json(),
-        ).sort((a, b) => (a.name > b.name ? 1 : -1)),
-      ))();
+      setEngines(await (await fetch(`/api/engines?${querify()}`)).json()))();
   }, []);
 
   return (
     <>
       <Header
-        onSearch={q => {
-          setResults([]);
-          let incomingResults: Result[] = [];
-          engines.map(async engine => {
-            const engineResults: Result[] = await (
-              await fetch(`/api/search?${querify({ engine: engine.id, q })}`)
-            ).json();
-            incomingResults = [...incomingResults, ...engineResults];
-            setResults(incomingResults);
-          });
+        onSearch={async q => {
+          dispatch(undefined);
+          const start = Date.now();
+          let slowestEngine: string | undefined;
+          await Promise.all(
+            Object.values(engines).map(async engine => {
+              dispatch({
+                engineId: engine.id,
+                results: await (
+                  await fetch(
+                    `/api/search?${querify({ engine: engine.id, q })}`,
+                  )
+                ).json(),
+              });
+              slowestEngine = engine.id;
+            }),
+          );
+          console.log(
+            `Slowest engine (${slowestEngine}) took ${Math.round(
+              Date.now() - start,
+            )}ms`,
+          );
         }}
       />
       <Sidebar engines={engines} />
-      <Results results={results} />
+      <Results engines={engines} resultGroups={resultGroups} />
     </>
   );
 };
