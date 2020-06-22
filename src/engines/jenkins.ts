9f398cdee96f8611ad48f11fa00a18f634b76a65
Art Chaidarun <artchaidarun@gmail.com>
2020-06-22 17:10:57 -0400

    Search Jenkins job descriptions

--- src/engines/jenkins.ts
+++ src/engines/jenkins.ts
@@ -1,25 +1,50 @@
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 
-let client: AxiosInstance | undefined;
+import { rateLimit } from "./index";
+
+interface Job {
+  description: string;
+  name: string;
+  url: string;
+}
+
+let getJobs: (() => Promise<Set<Job>>) | undefined;
 
 const engine: Engine = {
   id: "jenkins",
   init: ({ origin }: { origin: string }) => {
-    client = axios.create({ baseURL: origin });
+    const client = axios.create({ baseURL: origin });
+
+    getJobs = rateLimit(async () => {
+      // https://stackoverflow.com/a/25685928
+      const data: {
+        jobs: Pick<Job, "name">[];
+      } = (await client.get("/api/json")).data;
+      const jobs = data.jobs.map(j => j.name);
+      return new Set(
+        await Promise.all(
+          jobs.map<Promise<Job>>(
+            async name => (await client.get(`/job/${name}/api/json`)).data,
+          ),
+        ),
+      );
+    }, 4);
   },
   name: "Jenkins",
   search: async q => {
-    if (!client) {
+    if (!getJobs) {
       throw Error("Engine not initialized");
     }
 
-    // https://stackoverflow.com/a/25685928
-    const data: {
-      jobs: { name: string; url: string }[];
-    } = (await client.get("/api/json")).data;
-    return data.jobs
-      .filter(j => j.name.toLowerCase().includes(q.toLowerCase()))
+    return Array.from(await getJobs())
+      .filter(j =>
+        [j.name, j.description].some(s =>
+          s.toLowerCase().includes(q.toLowerCase()),
+        ),
+      )
+      .sort((a, b) => (a.name > b.name ? 1 : -1))
       .map(j => ({
+        snippet: j.description.length ? j.description : undefined,
         title: j.name,
         url: j.url,
       }));
