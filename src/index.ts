b95a30c39f7f33f895520d103f1cce1244e13d18
Rada OShea <47122551+radaduolingo@users.noreply.github.com>
2020-09-09 11:27:36 -0400

    Allow port override for metasearch (#1)

--- src/index.ts
+++ src/index.ts
@@ -127,7 +127,7 @@ ${JSON.stringify(data)}`);
 
   // Set up server
   const app = express();
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   app.use(compression());
   app.use(express.static("dist"));
 
