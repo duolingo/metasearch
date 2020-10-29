2cf1585677ee375cece4a7a933aedc292a53788e
Art Chaidarun <artchaidarun@gmail.com>
2020-10-29 16:59:13 -0400

    Survive Figma rate-limiting

--- src/engines/figma.ts
+++ src/engines/figma.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosInstance } from "axios";
 
 import { getUnixTime, rateLimit } from "../util";
 
-let getClient: (() => Promise<AxiosInstance>) | undefined;
+let getClient: (() => Promise<AxiosInstance | undefined>) | undefined;
 let orgId: string | undefined;
 
 const engine: Engine = {
@@ -19,22 +19,27 @@ const engine: Engine = {
     getClient = rateLimit(async () => {
       // Log into Figma using their web browser flow. Their session token seems
       // to expire after 1-3 days in my testing.
-      const tokenResponse = await axios.post(
-        "https://www.figma.com/api/session/login",
-        { email: user, password, username: user },
-        { headers: { "Content-Type": "application/json" } },
-      );
-      const token = (tokenResponse.headers["set-cookie"] as string[])
-        .find(c => /^figma\.st=[^;]/.test(c))
-        ?.split(/[=;]/)[1];
-      if (!token) {
-        throw Error("Figma login failed");
-      }
+      try {
+        const tokenResponse = await axios.post(
+          "https://www.figma.com/api/session/login",
+          { email: user, password, username: user },
+          { headers: { "Content-Type": "application/json" } },
+        );
+        const token = (tokenResponse.headers["set-cookie"] as string[])
+          .find(c => /^figma\.st=[^;]/.test(c))
+          ?.split(/[=;]/)[1];
+        if (!token) {
+          throw Error("Figma login failed");
+        }
 
-      return axios.create({
-        baseURL: "https://www.figma.com",
-        headers: { Cookie: `figma.st=${token}` },
-      });
+        return axios.create({
+          baseURL: "https://www.figma.com",
+          headers: { Cookie: `figma.st=${token}` },
+        });
+      } catch (ex) {
+        console.error(ex);
+        return undefined;
+      }
     }, 24);
 
     orgId = `${organization}`;
@@ -42,7 +47,7 @@ const engine: Engine = {
   name: "Figma",
   search: async q => {
     interface Model<M> {
-      getResult: (el: M, client: AxiosInstance) => Result | Promise<Result>;
+      getResult: (el: M) => Result | Promise<Result>;
       urlFragment: string;
     }
 
@@ -55,8 +60,17 @@ const engine: Engine = {
       url: string;
     }
 
+    // Get API client
+    if (!getClient) {
+      throw Error("Engine not initialized");
+    }
+    const client = await getClient();
+    if (!client) {
+      return [];
+    }
+
     /** Generates a string of HTML for displaying a linked thumbnail */
-    const getThumbnail = async (client: AxiosInstance, f: File) => {
+    const getThumbnail = async (f: File) => {
       try {
         await client.get(f.thumbnail_url, { maxRedirects: 0 });
         throw Error("Thumbnail URL not found");
@@ -76,38 +90,35 @@ const engine: Engine = {
 
     const MODEL_TYPES: Model<unknown>[] = [
       {
-        getResult: async ({ model }: { model: File }, client) => ({
+        getResult: async ({ model }: { model: File }) => ({
           modified: getUnixTime(model.updated_at),
           snippet: `File created by ${
             model.creator.handle
-          }<br>${await getThumbnail(client, model)}`,
+          }<br>${await getThumbnail(model)}`,
           title: model.name,
           url: model.url,
         }),
         urlFragment: "fig_files",
       },
       {
-        getResult: async (
-          {
-            file_count,
-            files_last_touched_at,
-            model,
-            recent_files,
-          }: {
-            file_count: number;
-            /** e.g. "2020-06-30T14:05:30.746Z" */
-            files_last_touched_at: string;
-            model: { id: string; name: string };
-            recent_files: File[];
-          },
-          client,
-        ) => ({
+        getResult: async ({
+          file_count,
+          files_last_touched_at,
+          model,
+          recent_files,
+        }: {
+          file_count: number;
+          /** e.g. "2020-06-30T14:05:30.746Z" */
+          files_last_touched_at: string;
+          model: { id: string; name: string };
+          recent_files: File[];
+        }) => ({
           modified: getUnixTime(files_last_touched_at),
           snippet: `Project containing ${
             file_count === 1 ? "1 file" : `${file_count} files`
           }<br>${(
             await Promise.all(
-              recent_files.slice(0, 3).map(f => getThumbnail(client, f)),
+              recent_files.slice(0, 3).map(f => getThumbnail(f)),
             )
           ).join("")}`,
           title: model.name,
@@ -140,11 +151,6 @@ const engine: Engine = {
     return (
       await Promise.all(
         MODEL_TYPES.map(async ({ getResult, urlFragment }) => {
-          if (!getClient) {
-            throw Error("Engine not initialized");
-          }
-
-          const client = await getClient();
           const data: {
             meta: { results: Parameters<typeof getResult>[0][] };
           } = (
@@ -157,9 +163,7 @@ const engine: Engine = {
               },
             })
           ).data;
-          return Promise.all(
-            data.meta.results.map(el => getResult(el, client)),
-          );
+          return Promise.all(data.meta.results.map(el => getResult(el)));
         }),
       )
     ).flat();
