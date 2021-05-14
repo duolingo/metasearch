ac30c86f9b5fb9319886903dbfbd16b4f54c0b66
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-05-15 01:52:28 +0200

    Results and links are displayed

--- src/engines/notion.ts
+++ src/engines/notion.ts
@@ -1,52 +1,71 @@
-// import { Client } from "@notionhq/client";
-const { Client } = require("@notionhq/client");
-let notion: typeof Client;
+
+import axios, { AxiosInstance } from "axios";
+import { getUnixTime } from "../util";
+
+let axiosClient: AxiosInstance | undefined;
+let notionWorkspace: string;
 
 const engine: Engine = {
   id: "notion",
-  init: ({ token }: { token: string }) => {
-    notion = new Client({ auth: token });
+  init: ({ token, workspace }: { token: string; workspace: string }) => {
+    token = token;
+    notionWorkspace = workspace;
+    axiosClient = axios.create({
+      baseURL: "https://api.notion.com/v1",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+        "Notion-Version": "2021-05-13",
+      },
+    });
   },
   name: "Notion",
   search: async (q) => {
-    if (!notion) {
+    if (!axiosClient) {
       throw Error("Engine not initialized");
     }
 
-    console.log("searching..");
-    const response = await notion.search({
-      query: q,
-      sort: {
-        direction: "ascending",
-        timestamp: "last_edited_time",
-      },
-    });
+    console.log("query for ", q)
 
-    console.log("notion response is... ", notion);
-    return response.results.map(() => ({
-      modified: new Date().getTime(),
-      snippet: "klsdjfdsf",
-      title: "dsfdsfd",
-      url: `https:dsfdsfds`,
-    }));
-    // return data.matches
-    //   .map((m) => m.metadata.metadata)
-    //   .filter((metadata) => !excludeRegex?.test(metadata.path_display))
-    //   .map((metadata) => ({
-    //     modified: getUnixTime(metadata.server_modified),
-    //     snippet: `${metadata[".tag"].charAt(0).toUpperCase()}${metadata[
-    //       ".tag"
-    //     ].slice(1)} in ${metadata.path_display
-    //       .slice(1)
-    //       .replace(/\/[^/]+$/, "")}`,
-    //     title: metadata.name,
-    //     url: `https://www.dropbox.com/home${metadata.path_display}`,
-    //   }));
+    const response = (
+      await axiosClient.post("/search",{}, {
+        data: {
+          query: q,
+          sort: {
+            direction: "ascending",
+            timestamp: "last_edited_time",
+          },
+          filter: {
+            object: 'page'
+          }
+        },
+      })
+    ).data;
 
-    // return {
-    //   modified: "test",
-    // };
+    console.log("notion response is... ", response);
+    const values = response.results.map((result: any) => {
+      const type: 'database' | 'page' = result.object;
+      const title  = result?.properties?.Name?.title[0]?.plain_text;
+      if (result.object === 'page' && title) {
+        console.log("title is", title)
+        return {
+          modified: getUnixTime(result.last_edited_time),
+          snippet: result.object,
+          title: title,
+          url:
+            type === "page"
+              ? `notion://notion.so/${notionWorkspace}/${formatTitle(
+                  title
+                )}-${formatId(result.id)}`
+              : "",
+        };
+      }
+    }).filter((o: any) => (o !== undefined));
+    return values
   },
 };
+
+const formatTitle = (title: string) => (title.replace(/\W+/g, '-'))
+const formatId = (id: string) => (id.replace(/-/g, ""))
 
 export default engine;
