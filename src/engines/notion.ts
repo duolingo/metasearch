64882e633940c297c425a6476f8995365639f287
tiankongguangzi@gmail.com <tiankongguangzi@gmail.com>
2021-05-15 10:18:37 +0200

    body params passed properly

--- src/engines/notion.ts
+++ src/engines/notion.ts
@@ -1,10 +1,20 @@
-
 import axios, { AxiosInstance } from "axios";
 import { getUnixTime } from "../util";
 
 let axiosClient: AxiosInstance | undefined;
 let notionWorkspace: string;
-
+// ,{}, {
+//         data: {
+//           query: q,
+//           sort: {
+//             direction: "ascending",
+//             timestamp: "last_edited_time",
+//           },
+//           filter: {
+//             object: 'page'
+//           }
+//         },
+//       })
 const engine: Engine = {
   id: "notion",
   init: ({ token, workspace }: { token: string; workspace: string }) => {
@@ -25,47 +35,59 @@ const engine: Engine = {
       throw Error("Engine not initialized");
     }
 
-    console.log("query for ", q)
+    console.log("query for ", q);
 
+    // const config = {
+    //   data: {
+    //     query: q,
+    //     sort: {
+    //       direction: "ascending",
+    //       timestamp: "last_edited_time",
+    //     },
+    //     filter: {
+    //       object: "page",
+    //     },
+    //   },
+    // };
     const response = (
-      await axiosClient.post("/search",{}, {
-        data: {
-          query: q,
-          sort: {
-            direction: "ascending",
-            timestamp: "last_edited_time",
-          },
-          filter: {
-            object: 'page'
-          }
+      await axiosClient.post("/search", {
+        query: q,
+        sort: {
+          direction: "ascending",
+          timestamp: "last_edited_time",
         },
+        // filter: {
+        //   object: "page",
+        // },
       })
     ).data;
 
     console.log("notion response is... ", response);
-    const values = response.results.map((result: any) => {
-      const type: 'database' | 'page' = result.object;
-      const title  = result?.properties?.Name?.title[0]?.plain_text;
-      if (result.object === 'page' && title) {
-        console.log("title is", title)
-        return {
-          modified: getUnixTime(result.last_edited_time),
-          snippet: result.object,
-          title: title,
-          url:
-            type === "page"
-              ? `notion://notion.so/${notionWorkspace}/${formatTitle(
-                  title
-                )}-${formatId(result.id)}`
-              : "",
-        };
-      }
-    }).filter((o: any) => (o !== undefined));
-    return values
+    const values = response.results
+      .map((result: any) => {
+        const type: "database" | "page" = result.object;
+        const title = result?.properties?.Name?.title[0]?.plain_text;
+        if (result.object === "page" && title) {
+          console.log("title is", title);
+          return {
+            modified: getUnixTime(result.last_edited_time),
+            snippet: result.object,
+            title: title,
+            url:
+              type === "page"
+                ? `notion://notion.so/${notionWorkspace}/${formatTitle(
+                    title
+                  )}-${formatId(result.id)}`
+                : "",
+          };
+        }
+      })
+      .filter((o: any) => o !== undefined);
+    return values;
   },
 };
 
-const formatTitle = (title: string) => (title.replace(/\W+/g, '-'))
-const formatId = (id: string) => (id.replace(/-/g, ""))
+const formatTitle = (title: string) => title.replace(/\W+/g, "-");
+const formatId = (id: string) => id.replace(/-/g, "");
 
 export default engine;
